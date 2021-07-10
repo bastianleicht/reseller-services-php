a9e6eb6bd91337ee71db176e153b5a6c993a026c
bastianleicht <mail@bastianleicht.de>
2021-07-10 13:23:09 +0200

    Payment: Added missing PHPDoc Comments

--- src/Payment.php
+++ src/Payment.php
@@ -19,24 +19,31 @@ class Payment
     }
 
     /**
+     * @param string $payment_method    SEPA | paysafecard | SOFORT | PAYPAL | GIROPAY | CREDITCARD | APPLEPAY
+     * @param float $amount             1.00
+     * @param string $success_url       https://interface.reseller-services.de/dashboard
+     * @param string $failure_url       https://interface.reseller-services.de/dashboard
+     * @param string|null $startPayment
+     * @return array|string
      * @throws GuzzleException
      */
-    public function create($payment_method, $amount, $success_url, $failure_url, $user_id, $startPayment = null)
+    public function create(string $payment_method, float $amount, string $success_url, string $failure_url, string $startPayment = null)
     {
         return $this->resellerServices->post('payment/create', [
             'startPayment' => $startPayment,
             'payment_method' => $payment_method,
             'amount' => $amount,
             'success_url' => $success_url,
-            'failure_url' => $failure_url,
-            'user_id' => $user_id
+            'failure_url' => $failure_url
         ]);
     }
 
     /**
+     * @param string $transaction_id    tr_yJ7GD6Mh3D
+     * @return array|string
      * @throws GuzzleException
      */
-    public function validate($transaction_id)
+    public function validate(string $transaction_id)
     {
         return $this->resellerServices->post('payment/check', [
             'tid' => $transaction_id
