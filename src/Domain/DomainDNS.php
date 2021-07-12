770334432da7f3f904853357a3423ae78db72bca
bastianleicht <mail@bastianleicht.de>
2021-07-12 14:08:01 +0200

    Domain: Implemented DNS

---
+++ src/Domain/DomainDNS.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright (c) 2021, Bastian Leicht <mail@bastianleicht.de>
+ *
+ * PDX-License-Identifier: BSD-2-Clause
+ */
+
+namespace ResellerServices\Domain;
+
+use GuzzleHttp\Exception\GuzzleException;
+use ResellerServices\ResellerServices;
+
+class DomainDNS
+{
+    private $resellerServices;
+
+    public function __construct(ResellerServices $resellerServices)
+    {
+        $this->resellerServices = $resellerServices;
+    }
+
+    /**
+     * @param string $domainName
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function get(string $domainName)
+    {
+        return $this->resellerServices->post('domain/dns/get', [
+            'domainName' => $domainName
+        ]);
+    }
+
+    /**
+     * @param string $domainName
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function update(string $domainName)
+    {
+        return $this->resellerServices->post('domain/dns/update', [
+            'domainName' => $domainName
+        ]);
+    }
+
+}
