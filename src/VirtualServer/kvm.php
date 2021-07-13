5d7d3193be3218e918c8c65cdb29ea07071af003
bastianleicht <mail@bastianleicht.de>
2021-07-13 08:59:23 +0200

    KVM: Implemented more stuff!

--- src/VirtualServer/kvm.php
+++ src/VirtualServer/kvm.php
@@ -49,14 +49,49 @@ class kvm
         return $this->kvmJobHandler;
     }
 
-    public function list()
+    /**
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function getTemplates()
     {
-        //TODO: Noch nicht in der API
+        return $this->resellerServices->post('rootserver/os/get', [ ]);
     }
 
-    public function get()
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function getDetails(string $server_id)
     {
-        //TODO: Noch nicht in der API
+        return $this->resellerServices->post('rootserver/getDetails', [
+            'vm_id' => $server_id
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function getStatus(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/getStatus', [
+            'vm_id' => $server_id
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function getConfig(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/getConfig', [
+            'vm_id' => $server_id
+        ]);
     }
 
     /**
@@ -76,6 +111,127 @@ class kvm
             'disk' => $disk,
             'ips' => $ips,
             'os' => $os
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @param int $cores
+     * @param int $memory
+     * @param int $disk
+     * @param int $ips
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function change(string $server_id, int $cores, int $memory, int $disk, int $ips)
+    {
+        //TODO: Server id nicht in API?
+        return $this->resellerServices->post('rootserver/change', [
+            'vm_id' => $server_id,
+            'cores' => $cores,
+            'memory' => $memory,
+            'disk' => $disk,
+            'ips' => $ips
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @param bool $force_delete
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function delete(string $server_id, bool $force_delete = false)
+    {
+        return $this->resellerServices->post('rootserver/delete', [
+            'vm_id' => $server_id,
+            'force' => $force_delete
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function start(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/manage/start', [
+            'vm_id' => $server_id
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function shutdown(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/manage/shutdown', [
+            'vm_id' => $server_id
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function stop(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/manage/stop', [
+            'vm_id' => $server_id
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function reboot(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/manage/reboot', [
+            'vm_id' => $server_id
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @param string $server_os
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function reinstall(string $server_id, string $server_os)
+    {
+        return $this->resellerServices->post('rootserver/manage/reinstall', [
+            'vm_id' => $server_id,
+            'os' => $server_os
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function resetPassword(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/manage/reset', [
+            'vm_id' => $server_id
+        ]);
+    }
+
+    /**
+     * @param string $server_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function noVNC(string $server_id)
+    {
+        return $this->resellerServices->post('rootserver/console/get', [
+            'vm_id' => $server_id
         ]);
     }
 
