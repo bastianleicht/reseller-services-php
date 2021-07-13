40c2e7cfc7b1cde3895e633458d48f39dbf381eb
bastianleicht <mail@bastianleicht.de>
2021-07-13 08:58:46 +0200

    KVM: Added kvmJobs

---
+++ src/VirtualServer/kvmJobs.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2021, Bastian Leicht <mail@bastianleicht.de>
+ *
+ * PDX-License-Identifier: BSD-2-Clause
+ */
+
+namespace ResellerServices\VirtualServer;
+
+use GuzzleHttp\Exception\GuzzleException;
+use ResellerServices\ResellerServices;
+
+class kvmJobs
+{
+    private $resellerServices;
+
+    public function __construct(ResellerServices $resellerServices)
+    {
+        $this->resellerServices = $resellerServices;
+    }
+
+    /**
+     * @param string $job_id
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function get(string $job_id)
+    {
+        return $this->resellerServices->post('rootserver/job/get', [
+            'job_id' => $job_id
+        ]);
+    }
+
+}
